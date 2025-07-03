d741d59bc5c17949b45b889e1940b81d695e7ead
Dynam011 <89169227+Dynam011@users.noreply.github.com>
2025-07-03 11:24:17 -0400

    update cors

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import userRoutes from "./routes/user.routes.js";
 import sectionsRoutes from "./routes/sections.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import assessment from "./routes/assessments.routes.js";
-import programs from "./routes/programs.routes.js"
-import inventory from "./routes/inventory.routes.js"
-import subjects from "./routes/subjects.routes.js"
-import enrollments from "./routes/enrollments.routes.js"
+import programs from "./routes/programs.routes.js";
+import inventory from "./routes/inventory.routes.js";
+import subjects from "./routes/subjects.routes.js";
+import enrollments from "./routes/enrollments.routes.js";
 import cors from "cors"; // <-- Cambia require por import
 
 import morgan from "morgan";
@@ -17,10 +17,12 @@ import morgan from "morgan";
 // Inicializa Express
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', // o '*' para permitir todos (solo en desarrollo)
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://culinary-school11.netlify.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api", userRoutes);
@@ -31,11 +33,12 @@ app.use("/api", assessment);
 app.use("/api", programs);
 app.use("/api", inventory);
 app.use("/api", subjects);
-app.use("/api",enrollments);
+app.use("/api", enrollments);
 
-pool.connect()
+pool
+  .connect()
   .then(() => console.log("Conexión exitosa a PostgreSQL"))
-  .catch(err => console.error("Error de conexión a PostgreSQL", err));
+  .catch((err) => console.error("Error de conexión a PostgreSQL", err));
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
